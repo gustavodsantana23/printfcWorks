91bd41b921c2b11f043ca927ec5c3d00596a5513
dino <gustavod.santana@gmail.com>
2014-03-21 01:52:10 -0300

    Mostrando conflitos pro Johnny

--- testando/src/helloWorld.java
+++ testando/src/helloWorld.java
@@ -1,6 +1,10 @@
-
 public class helloWorld {
-	public static void main(String[] args){
-		System.out.println("Hello World!");
+	public static void main(String[] args) {
+
+		System.out.println("Hello Printfc!!!");
+
+		int x = 10;
+		int y = 5;
+		int a = x + y;
 	}
 }
